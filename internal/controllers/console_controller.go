e95ae71686bbbb755157227dcc9abf131ceb2d8a
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-11-28 05:36:55 +0500

    Добавитл обработчик HandleConsoleCommand: обрабатывает запросы для командной строки ИД ошибки, которую он выдает  err: HCC-001

--- internal/controllers/console_controller.go
+++ internal/controllers/console_controller.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"fmt"
+	"todoapp/internal/db"
+)
+
+// Обрабатывает запросы для сомандной строки
+// err: HCC-001
+func HandleConsoleCommand(command string, todo_id int) {
+
+	if command == "show" {
+		id_rec, err := db.ReadAllRec()
+		if err != nil {
+			fmt.Printf("HCC-001: Ошибка: %v\n", err)
+		} else {
+			fmt.Printf("Прочитано: %v", id_rec)
+		}
+	}
+
+}
