5c25e6394eee9b5c7d20d858074d6a0ce246b7d5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-01 23:55:47 +0500

    Добавил json тэги

--- internal/models/TodoRecord.go
+++ internal/models/TodoRecord.go
@@ -4,10 +4,10 @@ import "database/sql"
 
 // Структура для записи задачи todo
 type TodoRecord struct {
-	ID               int
-	Data_of_creation sql.NullString
-	Date_max         sql.NullString
-	Todo_text        sql.NullString
-	Is_gone          int
-	Date_of_gone     sql.NullString
+	ID               int            `json:"id"`
+	Data_of_creation sql.NullString `json:"data_of_creation"`
+	Date_max         sql.NullString `json:"date_max"`
+	Todo_text        sql.NullString `json:"todo_text"`
+	Is_gone          int            `json:"is_gone"`
+	Date_of_gone     sql.NullString `json:"date_of_gone"`
 }
