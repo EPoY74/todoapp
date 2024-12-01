5d99a30b62650ac9cfee65e57f7451da7c753ec5
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-01 23:53:45 +0500

    Пробую вывести json на экран - не выводится

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"todoapp/internal/controllers"
 )
 
 func main() {
+
+	http.HandleFunc("/get_tasks", controllers.HandleTasks)
 
 	//Заглушка, первый код
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
+	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
