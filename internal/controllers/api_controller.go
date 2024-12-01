2ec36418293305274e62b621fc3e559f5d4e785e
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-01 23:54:33 +0500

    Контроллер для вывода ендпойнта со всеми записями из таблицы. Не работает пока

--- internal/controllers/api_controller.go
+++ internal/controllers/api_controller.go
@@ -1,1 +1,28 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"todoapp/internal/db"
+)
+
+func HandleTasks(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.Header().Set("Content-Type", "applications/json")
+		todoRecords, err := db.ReadAllRec()
+		//HT--001
+		if err != nil {
+			fmt.Printf("HT-001: Ошибка: %v\n", err)
+		}
+
+		data, err := json.Marshal(todoRecords)
+		if err != nil {
+			return
+		}
+		// for
+		json.NewEncoder(w).Encode(data)
+		return
+	}
+	w.WriteHeader((http.StatusMethodNotAllowed))
+}
