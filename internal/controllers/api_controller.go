b9e3fd8e1a239042adec195ac4cf79854866462c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-02 23:05:07 +0500

    Вывод json в браузер работает, с отступами

--- internal/controllers/api_controller.go
+++ internal/controllers/api_controller.go
@@ -9,19 +9,35 @@ import (
 
 func HandleTasks(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		w.Header().Set("Content-Type", "applications/json")
+		// text/plain"
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		// w.Header().Set("Content-Type", "applications/json")
+		// w.Header().Set("Content-Disposition", "inline")
 		todoRecords, err := db.ReadAllRec()
 		//HT--001
 		if err != nil {
 			fmt.Printf("HT-001: Ошибка: %v\n", err)
 		}
 
-		data, err := json.Marshal(todoRecords)
+		// data, err := json.Marshal(todoRecords)
+		// if err != nil {
+		// 	return
+		// }
+		// // for
+		// fmt.Printf("%v ", data)
+
+		// err = json.NewEncoder(w).Encode(todoRecords)
+		data, err := json.MarshalIndent(todoRecords, "", " ")
 		if err != nil {
+			http.Error(w,
+				//HT--002
+				fmt.Sprintf("HT--002: Ошибка при кодировании JSON: %v", err),
+				http.StatusInternalServerError)
+
 			return
 		}
-		// for
-		json.NewEncoder(w).Encode(data)
+
+		w.Write(data)
 		return
 	}
 	w.WriteHeader((http.StatusMethodNotAllowed))
