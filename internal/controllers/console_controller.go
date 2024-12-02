de5a1ee42a45f3891b5cabdb0150e36f5614702d
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-02 23:05:56 +0500

    Перевел вывод банных на экран из функии чтения из БД наружу

--- internal/controllers/console_controller.go
+++ internal/controllers/console_controller.go
@@ -16,7 +16,17 @@ func HandleConsoleCommand(command string, todo_id int) {
 		if err != nil {
 			fmt.Printf("HCC-001: Ошибка: %v\n", err)
 		} else {
-			fmt.Printf("Прочитано: %v", id_rec)
+			// fmt.Printf("Прочитано: %v", id_rec)
+			for _, todo := range id_rec {
+				fmt.Println(
+					todo.ID,
+					todo.Data_of_creation,
+					todo.Date_max,
+					todo.Todo_text,
+					todo.Is_gone,
+					todo.Date_of_gone,
+				)
+			}
 		}
 	} else {
 		fmt.Println("Пожалуйста, введите поддерживаемую команду.")
