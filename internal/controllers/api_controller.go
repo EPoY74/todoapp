9dd35d098c7a09012bcc3568ee93f49baaeae4c7
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-06 13:35:57 +0500

    Вынес перевеменны за main, сделал их глобальными для файла

--- internal/controllers/api_controller.go
+++ internal/controllers/api_controller.go
@@ -4,20 +4,56 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"todoapp/internal/db"
+	"todoapp/internal/models"
+)
+
+var (
+	todoRecords []models.TodoRecord
+	err         error
 )
 
 func HandleTasks(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method == http.MethodGet {
 		// text/plain"
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		// w.Header().Set("Content-Type", "applications/json")
 		// w.Header().Set("Content-Disposition", "inline")
-		todoRecords, err := db.ReadAllRec()
-		//HT--001
-		if err != nil {
-			fmt.Printf("HT-001: Ошибка: %v\n", err)
+		url_recs_id := r.URL.Query().Get("recs_id")
+		if url_recs_id != "" {
+			recs_id, err := strconv.Atoi(url_recs_id)
+			if err != nil {
+				//HT--003
+				fmt.Printf("HT-003: Ошибка: %v\n", err)
+			}
+			if recs_id > 0 {
+				fmt.Printf("Получаю запись %v \n", recs_id)
+				fmt.Fprintf(w, "Получаю запись %v \n", recs_id)
+				// w.Write(data)
+				return
+			}
+		} else {
+			todoRecords, err = db.ReadAllRec()
+			if err != nil {
+				fmt.Printf("HT-001: Ошибка: %v\n", err)
+			}
+
+			data, err := json.MarshalIndent(todoRecords, "", " ")
+			if err != nil {
+				http.Error(w,
+					//HT--002
+					fmt.Sprintf("HT--002: Ошибка при кодировании JSON: %v", err),
+					http.StatusInternalServerError)
+				return
+			}
+			w.Write(data)
+			return
+
 		}
+
+		//HT--001
 
 		// data, err := json.Marshal(todoRecords)
 		// if err != nil {
@@ -27,18 +63,7 @@ func HandleTasks(w http.ResponseWriter, r *http.Request) {
 		// fmt.Printf("%v ", data)
 
 		// err = json.NewEncoder(w).Encode(todoRecords)
-		data, err := json.MarshalIndent(todoRecords, "", " ")
-		if err != nil {
-			http.Error(w,
-				//HT--002
-				fmt.Sprintf("HT--002: Ошибка при кодировании JSON: %v", err),
-				http.StatusInternalServerError)
 
-			return
-		}
-
-		w.Write(data)
-		return
 	}
 	w.WriteHeader((http.StatusMethodNotAllowed))
 }
