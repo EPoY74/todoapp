6d33f25b9133a8b7507e94d10521e026039134b4
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-12-06 21:14:18 +0500

    Добавил функцию выдачи одной записи

--- internal/db/readData.go
+++ internal/db/readData.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -115,6 +116,82 @@ func ReadAllRec() ([]models.TodoRecord, error) {
 		if err != nil {
 			//id ReadRec--4
 			fmt.Printf("ReadRec--4: Не могу прочитать строку из БД %v", err)
+			fmt.Scanln()
+			continue
+		}
+		todoRecords = append(todoRecords, todo)
+	}
+
+	// for _, todo := range todoRecords {
+	// 	fmt.Println(
+	// 		todo.ID,
+	// 		todo.Data_of_creation,
+	// 		todo.Date_max,
+	// 		todo.Todo_text,
+	// 		todo.Is_gone,
+	// 		todo.Date_of_gone,
+	// 	)
+	// }
+	return todoRecords, nil
+}
+
+// Функция читает одну конкретную запись rec_id из  БД
+func ReadOneRec(rec_id int) ([]models.TodoRecord, error) {
+	// последний ReadRec--4
+	// Функция читает данные из БД
+	// id ReadOneRec--1
+	if rec_id < 0 {
+		return nil, errors.New(
+			"id ReadOneRec--1: недопустимый идентификатор записи")
+	}
+	fmt.Println("Привет! func ReadOneRec()")
+
+	db_connect, err := sql.Open("sqlite", "ep20231204_todo_cli.db")
+	// id ReadOneRec--2:
+	if err != nil {
+		fmt.Printf("ReadOneRec--2: Не могу открыть БД %v", err)
+		fmt.Scanln()
+		os.Exit(1)
+	}
+	defer db_connect.Close()
+
+	query := `
+		SELECT 
+			id,
+			data_of_creation,
+			date_max,
+			todo_text,
+			is_gone,
+			date_of_gone
+		FROM my_todo_list 
+		WHERE id = ?;
+	`
+	rows, err := db_connect.Query(query, rec_id)
+	// id ReadOneRec--3
+	if err != nil {
+		fmt.Printf("ReadOneRec--3: Не могу прочитать из БД %v", err)
+		fmt.Scanln()
+		os.Exit(1)
+	}
+	defer rows.Close()
+	todoRecords := []models.TodoRecord{}
+
+	// fmt.Printf("rows type is %T/n", rows)
+
+	for rows.Next() {
+		todo := models.TodoRecord{}
+
+		err := rows.Scan(
+			&todo.ID,
+			&todo.Data_of_creation,
+			&todo.Date_max,
+			&todo.Todo_text,
+			&todo.Is_gone,
+			&todo.Date_of_gone,
+		)
+		if err != nil {
+			//id ReadOneRec--4
+			fmt.Printf("ReadOneRec--4: Не могу прочитать строку из БД %v", err)
 			fmt.Scanln()
 			continue
 		}
